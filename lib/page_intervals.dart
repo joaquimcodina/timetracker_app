11c3f643cf29e10105b27e23d34298e23f6f4056
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-21 11:23:15 +0100

    Crear page_intervals.dart

---
+++ lib/page_intervals.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class PageIntervals extends StatefulWidget {
+  @override
+  _PageIntervalsState createState() => _PageIntervalsState();
+}
+
+class _PageIntervalsState extends State<PageIntervals> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
