79be41c4ec9fbb5763f1879047bdded67cbe9b3e
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2023-01-12 18:31:24 +0100

    Update requests.dart

--- lib/requests.dart
+++ lib/requests.dart
@@ -6,7 +6,7 @@ final http.Client client = http.Client();
 // better than http.get() if multiple requests to the same server
 
 // If you connect the Android emulator to the webserver listening to localhost:8080
-const String baseUrl = "https://4ade-37-14-46-30.ngrok.io";
+const String baseUrl = "https://a0c8-158-109-31-4.eu.ngrok.io";
 
 // If instead you want to use a real phone, you need ngrok to redirect
 // localhost:8080 to some temporal Url that ngrok.com provides for free: run
@@ -16,22 +16,26 @@ const String baseUrl = "https://4ade-37-14-46-30.ngrok.io";
 // mac download it from https://ngrok.com/. More on this here
 // https://medium.com/@vnbnews.vn/how-can-i-access-my-localhost-from-my-real-android-ios-device-d037fd192cdd
 
-Future<Tree> getTree(int id) async {
-  String uri = "$baseUrl/get_tree?$id";
-  final response = await client.get(Uri.parse(uri)); // updated 16-dec-2022
-  // response is NOT a Future because of await but since getTree() is async,
-  // execution continues (leaves this function) until response is available,
-  // and then we come back here
-  if (response.statusCode == 200) {
-    print("statusCode=$response.statusCode");
-    print(response.body);
-    // If the server did return a 200 OK response, then parse the JSON.
-    Map<String, dynamic> decoded = convert.jsonDecode(response.body);
-    return Tree(decoded);
+Future<Tree> getTree(int id, {String tag = 'null'}) async {
+  if (tag != 'null') {
+    return searchByTag(tag);
   } else {
-    // If the server did not return a 200 OK response, then throw an exception.
-    print("statusCode=$response.statusCode");
-    throw Exception('Failed to get children');
+    String uri = "$baseUrl/get_tree?$id";
+    final response = await client.get(Uri.parse(uri)); // updated 16-dec-2022
+    // response is NOT a Future because of await but since getTree() is async,
+    // execution continues (leaves this function) until response is available,
+    // and then we come back here
+    if (response.statusCode == 200) {
+      print("statusCode=$response.statusCode");
+      print(response.body);
+      // If the server did return a 200 OK response, then parse the JSON.
+      Map<String, dynamic> decoded = convert.jsonDecode(response.body);
+      return Tree(decoded);
+    } else {
+      // If the server did not return a 200 OK response, then throw an exception.
+      print("statusCode=$response.statusCode");
+      throw Exception('Failed to get children');
+    }
   }
 }
 
@@ -44,10 +48,17 @@ void manageResponse(response) {
   }
 }
 
-Future<void> searchByTag(String tag) async {
+Future<Tree> searchByTag(String tag) async {
   String uri = "$baseUrl/searchByTag?$tag";
   final response = await client.get(Uri.parse(uri));
-  manageResponse(response);
+  if (response.statusCode == 200) {
+    print("statusCode=$response.statusCode");
+    Map<String, dynamic> decoded = convert.jsonDecode(response.body);
+    return Tree(decoded);
+  } else {
+    print("statusCode=$response.statusCode");
+    throw Exception('Failed to get children');
+  }
 }
 
 Future<void> start(int id) async {
